8de66876df9a0f7bd35a8323fe864b7b639d66ae
danito <info@danito.net>
2022-04-28 08:20:07 +0700

    Create buzzer.sh

---
+++ scripts/buzzer.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# la8el by danito
+# (c) 2022 - Danny Ismarianto Ruhiyat
+
+# Astra DB's Build-A-Thon by DataStax and AngelHack
+
+timestamp=$(date '+%Y-%m-%d %H:%M:%S %Z')
+epoch=$(date -d "$timestamp" +"%s")
+
+# initializing the buzzer on GPIO6
+function buzzer_init {
+             if [ ! -f /sys/class/gpio/gpio6/direction ] ; then
+                sudo echo 6 > /sys/class/gpio/export
+             fi
+             sudo echo out > /sys/class/gpio/gpio6/direction
+         }
+
+# turning on the buzzer
+function buzzer_on {
+             sudo echo 1 > /sys/class/gpio/gpio6/value
+         }
+
+# turning off the buzzer
+function buzzer_off {
+             sudo echo 0 > /sys/class/gpio/gpio6/value
+         }
+
+buzzer_init
+
+if [ $1 == "on" ] || [ $1 == "ON" ] ; then
+   buzzer_on
+else
+   buzzer_off
+fi
